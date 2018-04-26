65f5778632d194ee525b26552be3ccca22eb1ecf
Sistemas Mosflor <34171440+sistemasmosflor@users.noreply.github.com>
2018-04-26 11:23:17 -0500

    inclusion de ingreso de feedback para su procesamiento

---
+++ resources/views/feedback_malo.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Feedback</title>
+    </head>
+    <body>
+        <form name="form" method="GET" action="">
+            <label>Que no te gusto?</label><br/>
+            <select name="razones">
+                <option>La seleccion de genero</option>
+                <option>La seleccion de pelicula/serie</option>
+            </select><br/>
+            <label>Danos una recomendacion de lo que no te gusto</label><br/>
+            <input type="text" name="recomendacion">
+        </form>
+    </body>
+</html>
